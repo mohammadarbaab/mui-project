f040ea6ca54185b43d5c7a739dacf91b05677c22
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-06-15 16:20:45 +0530

    add commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-// import {Typography} from '@mui/material'
-import './App.css';
-import Form from './components/Form';
+import { Home } from "@mui/icons-material";
+import Layout from "./components/layouts/Layout";
+import { Route,Routes,Router } from "react-router-dom";
+import Contact from './pages/Contact'
+import About from "./pages/About";
+import Menu from './pages/Menu';
 
+// import {Typography} from '@mui/material
 function App() {
   return (
     <div>
-    <Form />    
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layouts/Layout'
+
+const Contact = () => {
+  return (
+    <Layout>
+      <>Contact</>
+    </Layout>
+  )
+}
+
+export default Contact
